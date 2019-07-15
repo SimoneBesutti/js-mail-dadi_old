92be8309ba8e08da0f5d1bebb93c4dc7403ee990
simo <simonebesutti18@gmail.com>
2019-07-15 16:41:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ var email, elencoEmail, Messaggio, controlloemail, i;
 
 //Chiedere l'inserimento dell'email da parte dell'utente
 email = prompt("Inserisci la tua Email")
-document.getElementById("mioId").innerHTML = email;
+//document.getElementById("mioId").innerHTML = email;
 
 
 //Controllare se l'email fa parte della lista di chi può accedere
 elencoEmail = ["nome1@gmail.com", "nome2@gmail.com", "nome3@gmail.com", email];
-Messaggio = "Fanculo sei nell'elenco"
+Messaggio = "Benvenuto"
 
 //Stampare un essaggio appropriato
 for (var i = 0; i < elencoEmail.length; i++){
-    if (elencoEmail[i] ==email) 
-    console.log(elencoEmail[i]);
+    if (elencoEmail[i] == email) 
+    document.writeln(Messaggio);
 }
 
 
