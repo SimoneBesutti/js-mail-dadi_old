543447c4f3285057f8e5a900249571d7d6c82a2a
simo <simonebesutti18@gmail.com>
2019-07-15 19:38:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 //Dichiaro le variabili
-var email, elencoEmail, Messaggio, controlloemail, i;
+var email, elencoEmail, Messaggio, controlloemail, i, emailTrovata;
 
 
 //Chiedere l'inserimento dell'email da parte dell'utente
 email = prompt("Inserisci la tua Email")
-//document.getElementById("mioId").innerHTML = email;
 
+emailTrovata = false
 
 //Controllare se l'email fa parte della lista di chi può accedere
-elencoEmail = ["nome1@gmail.com", "nome2@gmail.com", "nome3@gmail.com", email];
-Messaggio = "Benvenuto"
+elencoEmail = ["nome1@gmail.com", "nome2@gmail.com", "nome3@gmail.com"];
+Messaggio = "Benvenuto";
 
 //Stampare un essaggio appropriato
 for (var i = 0; i < elencoEmail.length; i++){
-    if (elencoEmail[i] == email) 
-    document.writeln(Messaggio);
+    console.log(elencoEmail[i])
+
+    if (elencoEmail[i] == email)  { 
+        emailTrovata = true;
+        document.writeln(Messaggio);
+    }
+    
 }
 
+if (emailTrovata == false){
+    document.writeln("La tua mail non è stata trovata")
+}
 
 /*for (i = 0; i < elencoEmail.length; i++){
     document.writeln((i+1) + "; " + elencoEmail[i]);
